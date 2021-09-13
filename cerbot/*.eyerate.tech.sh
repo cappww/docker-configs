d0b29d8c78f630bb32e1653274e5d337db15e46c
Capp Wiedenhoefer <33261505+cappww@users.noreply.github.com>
2021-09-13 11:24:12 -0700

    update

--- cerbot/*.eyerate.tech.sh
+++ cerbot/*.eyerate.tech.sh
@@ -1,8 +1,8 @@
+# If you want to run this outside of the intended webserver, include the --manual option
 sudo docker run -it --rm --name certbot \
 	-v "/etc/letsencrypt:/etc/letsencrypt" \
 	-v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
 	certbot/certbot certonly \
 		--server https://acme-v02.api.letsencrypt.org/directory \
-		--manual \
-		-d *.eyerate.tech \
+		-d <domain-name.com> \
 		--preferred-challenges dns
