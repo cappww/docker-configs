5b94addc26009385662c23b8106a6994b3784ff5
cappww <cappw1@gmail.com>
2021-11-01 10:06:18 -0700

    Make changes to nginx

--- nginx/simple-app-2/src/main.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("simple-server-2");
-});
-
-app.listen(5001, () => console.log("simple-server-2 is up and running"));
--- nginx/simple-app/src/main.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("simple-server-1");
-});
-
-app.listen(5000, () => console.log("simple-server-1 is up and running"));
